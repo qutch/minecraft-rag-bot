4d130517f7e4cbfc831aa2aff7139635416bcaa6
Hutch Turner <hutchinson.turner@gmail.com>
2025-10-24 14:04:28 -0400

    having issues with timout

--- frontend/src/chat.jsx
+++ frontend/src/chat.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 export default function ChatSection() {
     return (
         <div className="flex flex-col">
@@ -7,7 +9,27 @@ export default function ChatSection() {
     )
 }
 
+const messages = [];
+
 function ChatBox() {
+
+    const [query, setQuery] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    const fetchAnswer = async () => {
+        const params = new URLSearchParams({ q: query })
+        const response = await fetch(`http://localhost:8000/ask?${params}`)
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+
+        const data = await response.json()
+        console.log(data)
+
+        setAnswer(data)
+    }
+
     return (
         <div className='flex flex-row w-screen justify-center p-10'>
             <div className='flex flex-col items-end rounded-[20px] w-150 p-5 bg-[#2e2e2e] shadow-gray-700/20 shadow-lg border-1 border-emerald-600/30 border-opacity-30'>
@@ -15,8 +37,10 @@ function ChatBox() {
                     rows='2'
                     placeholder="What do you wanna know?"
                     className='z-0 outline-none field-sizing-content text-gray-100 text-xl resize-none w-[100%] max-h-[300px] font-outfit font-[300]'
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
                 />
-                <button className="flex flex-col justify-center bg-blue-400 rounded-[50px] hover:bg-orange-500 h-10 w-20 z-10 font-outfit font-[500]">Enter ➤</button>
+                <button className="flex flex-col justify-center bg-blue-400 rounded-[50px] hover:bg-orange-500 h-10 w-20 z-10 font-outfit font-[500]" onClick={fetchAnswer}>Enter ➤</button>
             </div>
         </div>
     )
